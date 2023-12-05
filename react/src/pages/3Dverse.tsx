260946ce556b43b83e47755d80be7d4fa418e47e
Maquereau <macrorush@gmail.com>
2023-12-05 16:27:24 +0100

    update : added a bunch of things 🐱‍🐉

--- react/src/pages/3Dverse.tsx
+++ react/src/pages/3Dverse.tsx
@@ -3,11 +3,13 @@ import { useScript } from '@uidotdev/usehooks';
 import AppConfig from '../_3dverseEngine/AppConfig';
 import { Joystick } from 'react-joystick-component'; 
 import { _SDK3DVerse } from '../_3dverseEngine/declare';
+import {Character} from "../components/character";
 import './3Dverse.scss';
 
 declare const SDK3DVerse : typeof _SDK3DVerse;
 
 export const Canvas3Dverse = () => {
+
   const status = useScript(
     `https://cdn.3dverse.com/legacy/sdk/latest/SDK3DVerse.js`,
     {
@@ -16,14 +18,18 @@ export const Canvas3Dverse = () => {
   );
 
   const initApp = useCallback(async () => {
+    const character = new Character(SDK3DVerse);
     await SDK3DVerse.joinOrStartSession({
       userToken : AppConfig.USER_TOKEN,
       sceneUUID : AppConfig.SCENE_UUID,
       canvas : (document.getElementById('display-canvas') as HTMLElement),
-      viewportProperties : {
-        defaultControllerType : SDK3DVerse.controller_type.editor,
-      },
+      // viewportProperties : {
+      //   defaultControllerType : SDK3DVerse.controller_type.none,
+      // },
+      createDefaultCamera: false,
+      startSimulation: "on-assets-loaded"
     });
+    await character.InitFirstPersonController("ad535b41-11a8-4bee-95a9-9fc89d1b6c99");
   }, []);
 
   useEffect(() => {
