64ac42c205a25c2f28081495ca3864f5c5047bd4
Gwenitora <gwenitora.gardu74@gmail.com>
2023-12-07 05:11:06 +0100

    Revert "Feature: first UI"
    This reverts commit f40fd9bd51c0c92a730f4c595b38ceef5d8ccdc2.

--- src/components/inventory.tsx
+++ src/components/inventory.tsx
@@ -1,5 +1,4 @@
 import AppConfig from '../_3dverseEngine/AppConfig';
-import './inventory.scss';
 
 type Object = 
 {
@@ -21,7 +20,7 @@ export class Inventory
         this.caseTexture = texture;
     }
 
-    public insert(object:Object)
+    public insert(object : Object)
     {
         if (this.size > this.array.length)
         {
@@ -34,25 +33,30 @@ export class Inventory
     public insertList(objects:Object[])
     {
         for (let index = 0; index < objects.length; index++) {
-            this.insert(objects[index]);
+            this.array.push(objects[index]);
+            
         }
     }
 
-    public delete(object:Object | number, onlyFirst:boolean = true) 
+    public delete(object : Object | number, onlyFirst : boolean = true) 
     {
         for (let index = 0; index < this.array.length; index++) {
-            if ((object as Object).UUID ? this.array[index].UUID === (object as Object).UUID:this.array[index].UUID === object)
+            if ((object as Object).UUID ? this.array[index].UUID === (object as Object).UUID : this.array[index].UUID === object)
             {
                 this.array.splice(index, 1) // number of element wich are delete at index
-                if (onlyFirst) return;
             }
         }
     }
 
-    public deleteList(objects:(Object | number)[], onlyFirst:boolean = true) 
+    public deleteList(objects : (Object | number)[], onlyFirst : boolean = true) 
     {
         for (let index = 0; index < objects.length; index++) {
-            this.delete(objects[index], onlyFirst)
+            for (let index2 = 0; index2 < this.array.length; index2++) {
+                if (this.array[index2] === objects[index])
+                {
+                    this.array.splice(index2, 1) // number of element wich are delete at index
+                }
+            }
         }
     }
 
@@ -65,14 +69,14 @@ export class Inventory
             <div className='inv'>
                 {
                     this.array.map((e, i) => {
-                        return <img key={i} className='inventory item' src={e.texture} alt={`Image of the item ${e.name} (uuid:${e.UUID})`} />
+                        return <img className='inventory item' src={e.texture} alt={`Image of the item ${e.name} (uuid: ${e.UUID})`} />
                     })
                 }
             </div>
             <div className='inv'>
                 {
                     this.array.map((e, i) => {
-                        return <img key={i} className='inventory case' src={this.caseTexture} alt="Image of case of inventory" />
+                        return <img className='inventory case' src={this.caseTexture} alt="Image of case of inventory" />
                     })
                 }
             </div>
@@ -81,12 +85,12 @@ export class Inventory
 }
 
 export const InventoryReact = () => {
-    const inventory:Inventory = new Inventory(10, `${AppConfig.HOST}:${AppConfig.PORT}/img/case.png`);
-    const object:Object = {name:"ampoule", UUID:0, texture:`${AppConfig.HOST}:${AppConfig.PORT}/img/ampoule.png`};
-    const object2:Object = {name:"ampoule2", UUID:1, texture:`${AppConfig.HOST}:${AppConfig.PORT}/img/ampoule.png`};
-    const object3:Object = {name:"ampoule3", UUID:2, texture:`${AppConfig.HOST}:${AppConfig.PORT}/img/ampoule.png`};
-    const object4:Object = {name:"ampoule4", UUID:3, texture:`${AppConfig.HOST}:${AppConfig.PORT}/img/ampoule.png`};
-    const object5:Object = {name:"gameBible", UUID:4, texture:`https://static.vecteezy.com/system/resources/previews/009/399/398/original/old-vintage-book-clipart-design-illustration-free-png.png`};
+    const inventory : Inventory = new Inventory(10, `${AppConfig.HOST}:${AppConfig.PORT}/img/case.png`);
+    const object : Object = {name : "ampoule", UUID : 0, texture : `${AppConfig.HOST}:${AppConfig.PORT}/img/ampoule.png`};
+    const object2 : Object = {name : "ampoule2", UUID : 1, texture : `${AppConfig.HOST}:${AppConfig.PORT}/img/ampoule.png`};
+    const object3 : Object = {name : "ampoule3", UUID : 2, texture : `${AppConfig.HOST}:${AppConfig.PORT}/img/ampoule.png`};
+    const object4 : Object = {name : "ampoule4", UUID : 3, texture : `${AppConfig.HOST}:${AppConfig.PORT}/img/ampoule.png`};
+    const object5 : Object = {name : "gameBible", UUID : 4, texture : `https://static.vecteezy.com/system/resources/previews/009/399/398/original/old-vintage-book-clipart-design-illustration-free-png.png`};
 
     inventory.insertList([object, object2, object3, object4, object5]);
     inventory.insertList([object, object2, object3, object4, object5]);
