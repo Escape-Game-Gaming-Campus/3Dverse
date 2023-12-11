31a251545805726079ba443e620ea38a0dd760a7
Maquereau2 <macrorush@gmail.com>
2023-12-11 15:31:17 +0100

    update : added css 😢

--- react/src/components/digicode.tsx
+++ react/src/components/digicode.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './digicode.scss';
 interface DigicodeProps {
     onClose: () => void;
     onDigitPress: (digit: string) => void;
@@ -29,27 +29,30 @@ interface DigicodeProps {
     };
   
     return (
-      <div>
-        <div>{enteredCode}</div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <button onClick={() => handleDigitPress('1')}>1</button>
-          <button onClick={() => handleDigitPress('2')}>2</button>
-          <button onClick={() => handleDigitPress('3')}>3</button>
+      <div className="digicode">
+        <div className="code-display">{enteredCode}</div>
+        <div className="button-container">
+          <div className="row">
+            <button className="digicode-button" onClick={() => handleDigitPress('1')}>1</button>
+            <button className="digicode-button" onClick={() => handleDigitPress('2')}>2</button>
+            <button className="digicode-button" onClick={() => handleDigitPress('3')}>3</button>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <button onClick={() => handleDigitPress('4')}>4</button>
-          <button onClick={() => handleDigitPress('5')}>5</button>
-          <button onClick={() => handleDigitPress('6')}>6</button>
+          <div className="row">
+            <button className="digicode-button" onClick={() => handleDigitPress('4')}>4</button>
+            <button className="digicode-button" onClick={() => handleDigitPress('5')}>5</button>
+            <button className="digicode-button" onClick={() => handleDigitPress('6')}>6</button>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <button onClick={() => handleDigitPress('7')}>7</button>
-          <button onClick={() => handleDigitPress('8')}>8</button>
-          <button onClick={() => handleDigitPress('9')}>9</button>
+          <div className="row">
+            <button className="digicode-button" onClick={() => handleDigitPress('7')}>7</button>
+            <button className="digicode-button" onClick={() => handleDigitPress('8')}>8</button>
+            <button className="digicode-button" onClick={() => handleDigitPress('9')}>9</button>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <button onClick={handleClear}>Clear</button>
-          <button onClick={handleSubmit}>Submit</button>
+          <div className="row">
+            <button className="digicode-button" onClick={() => handleDigitPress('0')}>0</button>
+          </div>
+          <div className="row">
+            <button className="digicode-button" onClick={handleClear}>Clear</button>
+            <button className="digicode-button" onClick={handleSubmit}>Submit</button>
           </div>
         </div>
       </div>
